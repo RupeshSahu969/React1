de1bbe1a48864be269b3525e8d77a9bbcbdb9c3d
Rupesh Kumar <rupeshsahu969@gmail.com>
2022-05-16 21:42:04 +0530

    add

---
+++ src/component/anser1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Answer = ({name,place}) => {
+
+    return 
+    (
+    <div> Hello from {name} I'm From {place}</div>
+    );
+}
+
+export default Answer;
---
+++ src/component/hello.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+
+const Hello = () => {
+
+    return <div> 
+        <h1>Oprating system</h1>
+        <ul>
+            <li>Android</li>
+            <li>Samsang</li>
+            <li>apple</li>
+            <li>iphone</li>
+        </ul>
+
+        <h1>Mobile Manufacharuing</h1>
+        <ul>
+            <li>Android</li>
+            <li>Samsang</li>
+            <li>apple</li>
+            <li>iphone</li>
+        </ul>
+         </div>
+}
+
+export default Hello;
